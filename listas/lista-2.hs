d3cdb4d3473283861baabd02384b7bef816e4c72
Pierre Oriá <108680221+pierreoria@users.noreply.github.com>
2024-04-29 15:41:38 -0300

    Update lista-2.hs

--- listas/lista-2.hs
+++ listas/lista-2.hs
@@ -76,3 +76,62 @@ bin (n,k)
     | otherwise = bin ((n-1),k) + bin((n-1),(k-1))
 
 
+fibonacci :: Int -> Int
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci n = fib' (n-2) 0 1
+  where
+    fib' 0 x y = x + y
+    fib' i x y = fib' (i-1) y (x+y)
+
+{-
+11. Os números de Tribonacci são dados pelas seguintes equações
+T(1) = 1
+T(2) = 1
+T(3) = 2
+T(n + 1) = T(n) + T(n − 1) + T(n − 2)
+
+Implemente uma função recursiva eficiente que calcula T n. Considere o uso de uma
+função auxiliar.
+-}
+
+trib :: Int -> Integer
+trib n = trib' n (0, 0, 1)
+  where
+    trib' 0 (a, _, _) = a
+    trib' 1 (_, b, _) = b
+    trib' 2 (_, _, c) = c
+    trib' n (a, b, c) = trib' (n - 1) (b, c, a + b + c)
+
+addEspacos :: Int -> String
+addEspacos n = aux' n ""
+    where
+        aux' 0 x = x
+        aux' n x = aux' (n-1) (x ++ " ")
+
+{-
+    Defina a função paraDireita utilizando a definição de addEspacos para adicionar uma
+    quantidade n de espaços à esquerda de um dado String, movendo o mesmo para a direita.
+    paraDireita :: Int −> String −> String
+-}
+
+paraDireita :: Int -> String -> String
+paraDireita n s = aux' n s
+    where
+        aux' 0 s = s
+        aux' n s = aux' (n-1) (" " ++ s) 
+
+{-
+
+    Escreva uma função para retornar, em forma de tabela, todas as vendas da semana 0 até
+    a semana n, incluindo o total e a média de vendas no período. Usem as funções definidas
+    previamente e defina novas funções que achar necessário.
+    Semana Venda
+    0 12
+    1 14
+    2 15
+    Total 41
+    Média 13.6667
+
+
+-}
